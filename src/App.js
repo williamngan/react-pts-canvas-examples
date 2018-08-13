64c2e9886d3600a3e3bb00832c0de523140d11a5
William Ngan <williamngan@users.noreply.github.com>
2018-08-12 20:51:19 -0700

    Update react and create new examples

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-import PtsChart from './PtsChart.jsx'
+import PtsCanvas from "./PtsCanvas";
+import {ChartExample, AnimationExample} from './PtsExamples';
 import './App.css';
+import './highlight-github.css';
+import Highlight from 'react-highlight'
 
 class App extends Component {
   constructor( props ) {
     super( props );
 
     this.state = {
-      variance: 0.2
+      variance: 0.2,
+      pauseAnimation: false
     }
 
     this.mockData( 0.2 );
@@ -30,20 +34,91 @@ class App extends Component {
     this.setState({variance: event.target.value});
   }
 
+  handleClick(event) {
+    this.setState({pauseAnimation: !this.state.pauseAnimation});
+  }
+
   componentWillUpdate(nextProps, nextState) {
+    console.log("will update", nextState);
     this.mockData( nextState.variance );
   }
 
   render() {
     return (
+      
       <div className="App" style={{textAlign: "left"}}>
-        <h1>Example of using Pts in React</h1>
-        <div >
-          <PtsChart data={this.chartData}  />
-          <PtsChart bgcolor="#6fa" data={this.chartData}  />
+        <a id="repo" href="https://github.com/williamngan/pts-react-example">Github</a>
+        <div className="top">
+          <h1>Pts + React</h1>
+          
+          <h2>
+            <a href="https://ptsjs.org" target="pts">Pts</a> is a library for visualization and creative-coding. 
+            You can incorporate it into React to create canvas animations and other fun things.
+            Here are a few examples to help you get started.
+          </h2>
+
+          <p>Note that Pts is an ES6 library, and for production (minified) build you'll need to set up custom build steps. Hopefully <a href="https://github.com/facebook/create-react-app/issues/4421" target="pts">this issue</a> should be fixed in the next release of React.</p>
         </div>
-        <label>Variance (in steps of 0.05): <input type="number" value={this.state.variance} onChange={this.handleChange.bind(this)} min={0} step={0.05} /></label>
-        <a href="https://github.com/williamngan/pts-react-example">Source code</a>
+      
+        <div className="row">
+          <div><PtsCanvas /></div>
+          <div>
+            <h3>PtsCanvas</h3>
+            <p>PtsCanvas is a basic implementation of Pts in a React component.</p>
+            <p>A quick way to build your own Pts component is to extend the PtsCanvas class, and then override the <code>animate</code> function (and optionally <code>start</code>, <code>action</code>, and <code>resize</code> functions.)</p>
+            <Highlight className="xml">{`<PtsCanvas />`}</Highlight>
+            <p>Default properties: </p>
+            <Highlight className="js">
+{`{
+  name: "pt",
+  background: "#9ab",
+  resize: true,
+  retina: true,
+  loop: true
+}`}          
+          </Highlight>
+          <p><a href="https://github.com/williamngan/pts-react-example/blob/master/src/PtsChart.jsx">Source Code</a></p>
+          </div>
+        </div>
+
+
+        <div className="row">
+          <div><AnimationExample name="pts_anim" background="#fe3" pause={this.state.pauseAnimation} /></div>
+          <div>
+            <h3>AnimationExample Component</h3>
+            <p>A component that renders a continuous noise pattern. (Might be good as a fancy loading animation.)</p>
+            <p>Hover over the canvas to change the animation, and toggle Play/Pause by clicking this button: </p>
+            <p><button onClick={this.handleClick.bind(this)}>{this.state.pauseAnimation ? "Play" : "Pause"}</button></p>
+            <Highlight className="xml">
+{`<AnimationExample 
+    name="pts_anim"
+    background="#fe3"
+    pause={this.state.pauseAnimation} 
+/>`}
+            </Highlight>
+            <p>AnimationExample extends PtsCanvas and overrides the <code>animate</code> function to implement a custom animation. It only takes a couple lines of code!</p>
+            <p><a href="https://github.com/williamngan/pts-react-example/blob/master/src/PtsExample.jsx#L59">Source Code</a></p>
+          </div>
+        </div>
+
+        <div className="row">
+          <div><ChartExample name="pts_chart" background="#0c9" loop={false} data={this.chartData} variance={this.state.variance} /></div>
+          <div>
+            <h3>ChartExample Component</h3>
+            <p>You can also use Pts to build a custom visualization component. The following example draws a bell curve on canvas. Change the variance property to update the visualization:</p>
+            <p><label>Variance: <input type="range" value={this.state.variance} min={0.05} max={5} step={0.05} onChange={this.handleChange.bind(this)} /></label> ({this.state.variance})</p>
+            <Highlight className="xml">
+{`<ChartExample 
+    name="pts_chart" 
+    background="#0c9" loop={false} 
+    data={this.chartData} 
+    variance={this.state.variance} 
+/>`}
+            </Highlight>
+            <p><a href="https://github.com/williamngan/pts-react-example/blob/master/src/PtsExample.jsx#L8">Source Code</a></p>
+          </div>
+        </div>
+        
       </div>
     );
   }
---
+++ src/PtsExamples.jsx
@@ -0,0 +1,109 @@
+import {Pt, Group, Line, Create} from 'pts';
+import PtsCanvas from "./PtsCanvas";
+
+
+/**
+ * Chart example component, which extends PtsCanvas
+ */
+export class ChartExample extends PtsCanvas {
+
+  _renderChart() {
+    // Given the data, distribute bars across the space's size
+    let w = (this.space.size.x) / this.props.data.length;
+    let bars = this.props.data.map( (d,i) => {
+      return new Group( new Pt(i*w, this.space.size.y), new Pt( i*w, this.space.size.y-d*this.space.size.y-1) );
+    });
+    
+    // Draw a line controlled by mouse pointer
+    let line = new Group(new Pt(0, this.space.pointer.y), new Pt( this.space.size.x, this.space.pointer.y ) );
+    this.form.stroke("#fff", 3).line( line, 10, "circle" );
+
+    // Draw the bars and also check intersection with the pointer's line
+    let intersects = bars.map( (b, i) => {
+      this.form.stroke("#123",w-1).line( bars[i]  );
+      return Line.intersectLine2D( b, line )
+    });
+
+    // Draw intersection points
+    this.form.fillOnly("#f6c").points( intersects, w/2 );
+  }
+
+
+  // Override PtsCanvas' animate function
+  animate(time, ftime) {
+    this._renderChart();
+  }
+
+
+  // Override PtsCanvas' action function
+  action(type, x, y) {
+    this.space.clear(); // since we're not animating continuously, manually clear canvas and re-render chart
+    this._renderChart();
+  }
+
+
+  // Override PtsCanvas' resize function
+  resize( size, evt ) {
+    if (this.form.ready) {
+      this.space.clear();
+      this._renderChart();
+    }
+  }
+
+}
+
+
+/**
+ * Chart example component, which extends PtsCanvas
+ */
+export class AnimationExample extends PtsCanvas {
+
+  constructor() {
+    super();
+    this.noiseGrid = [];
+  }
+
+  _create() {
+    // Create a line and a grid, and convert them to `Noise` points
+    let gd = Create.gridPts( this.space.innerBound, 20, 20 );
+    this.noiseGrid = Create.noisePts( gd, 0.05, 0.1, 20, 20 );
+  }
+
+  componentDidUpdate() {
+    if (this.props.pause) {
+      this.space.pause();
+    } else {
+      this.space.resume();
+    }
+  }
+
+
+  // Override PtsCanvas' start function
+  start(space, bound) {
+    this._create();
+  }
+
+
+  // Override PtsCanvas' resize function
+  resize() {
+    this._create();
+  }
+
+
+  // Override PtsCanvas' animate function
+  animate(time, ftime) {
+
+    if (!this.noiseGrid) return;
+
+    // Use pointer position to change speed
+    let speed = this.space.pointer.$subtract( this.space.center ).divide( this.space.center );
+
+    // Generate noise in a grid
+    this.noiseGrid.forEach( (p) => {
+      p.step( 0.01*speed.x, 0.01*(1-speed.y) );
+      this.form.fillOnly("#123").point( p, Math.abs( p.noise2D() * this.space.size.x/18 ), "circle" );
+    });
+
+  }
+
+}
